5e49919fe75723f09b060fbc873f1f45e722b199
Deanna Arora <aroradeanna2@gmail.com>
2018-05-12 14:35:39 +1000

    Added Stemmer library to get stemmed words like snow from snowing

--- extractKeywords.py
+++ extractKeywords.py
@@ -1,10 +1,11 @@
 import nltk
 from nltk import word_tokenize, pos_tag
+from nltk.stem.snowball import SnowballStemmer
 import string
 from collections import Counter
 
-quote = "OMG had the best food ever at the FB Hackathon!"
 def getKeywords(quote):
+    stemmer = SnowballStemmer("english")
     # Breaks sentences into segments
     words = word_tokenize(str(quote))
     striped_words = []
@@ -13,10 +14,9 @@ def getKeywords(quote):
         # Removes punctuation
         for punc in string.punctuation:
             word = word.strip().replace(punc, '')
-
-        if 'ing' in word:
-            word = word.replace('ing', '')
-        if word !=  '':
+        if word.endswith("ing") or word.endswith("ed") or  word.endswith("er"):
+            word = stemmer.stem(word)
+        if word !=  '' and len(word) > 1:
             striped_words.append(word)
 
     # Assigns Pos Tags
@@ -28,9 +28,7 @@ def getKeywords(quote):
     final = []
     for key, values in posWords.items():
         # Select Nouns and Proper Nouns (singular)
-        if key[1] == "NNP" or key[1] == "NN" or key[1] == "VBG":
+        if key[1] == "NNP" or key[1] == "NN" or key[1] == "VBG" or key[1] == "JJ" or key[1] == "VBD" :
             final.append(key[0])
     # print(final)
     return final
-
-# getKeywords(quote)
