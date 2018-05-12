627aeba5d19283fa8f477c27e96002ae61af6cd4
Deanna Arora <aroradeanna2@gmail.com>
2018-05-12 11:36:10 +1000

    Removing 'ing's anf actually getting puncuation removal working.

--- extractKeywords.py
+++ extractKeywords.py
@@ -3,6 +3,7 @@ from nltk import word_tokenize, pos_tag
 import string
 from collections import Counter
 
+quote = "OMG had the best food ever at the FB Hackathon!"
 def getKeywords(quote):
     # Breaks sentences into segments
     words = word_tokenize(str(quote))
@@ -12,19 +13,24 @@ def getKeywords(quote):
         # Removes punctuation
         for punc in string.punctuation:
             word = word.strip().replace(punc, '')
-        striped_words.append(word)
+
+        if 'ing' in word:
+            word = word.replace('ing', '')
+        if word !=  '':
+            striped_words.append(word)
 
     # Assigns Pos Tags
-    print(nltk.pos_tag(words))
+    # print(nltk.pos_tag(striped_words))
 
     # Remove duplicates
-    posWords = Counter(nltk.pos_tag(words))
+    posWords = Counter(nltk.pos_tag(striped_words))
 
     final = []
     for key, values in posWords.items():
         # Select Nouns and Proper Nouns (singular)
-        if key[1] == "NNP" or key[1] == "NN":
+        if key[1] == "NNP" or key[1] == "NN" or key[1] == "VBG":
             final.append(key[0])
-
-
+    # print(final)
     return final
+
+# getKeywords(quote)
