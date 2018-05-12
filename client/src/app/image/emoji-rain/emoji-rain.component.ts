a7d805fa48738ea4ac20561cc741d7b79d5e6778
Alexander Edwards <alexedwardsxyz@gmail.com>
2018-05-12 13:58:36 +1000

    Working Emoji from backend

--- client/src/app/image/emoji-rain/emoji-rain.component.ts
+++ client/src/app/image/emoji-rain/emoji-rain.component.ts
@@ -15,15 +15,18 @@ export class EmojiRainComponent implements OnInit {
   }
 
   pickRandomEmoji(){
-    // Just pick a random item from 'this.emojis'
+    if (this.emojis.length != 0) {
+      return this.emojis[Math.floor(Math.random()*this.emojis.length)]
+    }
+    return ''
   }
 
 
   ngOnInit() {
     let post = document.getElementById("post");
     let delay = 180;
-    function doSetTimeout(x, loop) {
-      setTimeout(function () {
+    let doSetTimeout = (x, loop) => {
+      setTimeout(() => {
         let div = document.createElement('div');
         div.setAttribute("class", "smile");
         div.style.left = `${x}px`;
@@ -39,7 +42,7 @@ export class EmojiRainComponent implements OnInit {
           div.remove()
         }, 5000);
       }, loop * delay);
-    }
+    };
     let n_to_create = 10000;
     for (let i = 0; i < n_to_create; i++) {
       var x = Math.random() * post.offsetWidth;
